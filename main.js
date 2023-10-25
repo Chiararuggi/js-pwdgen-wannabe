5ca33128ab2a7ffe4cbe7609ece2f904e24e1ff8
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-10-25 15:52:09 +0200

    exercise done

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const nomeUtente = prompt( "Inserisci il tuo nome");
+const eta = prompt("Inserisci la tua età");
+const colore = prompt("Inserisci il tuo colore preferito");
+let suffix = '#109'
+const password = nomeUtente + eta + colore + suffix;
+
+document.getElementById("titolo").innerHTML = "Benvenuto/a, " + nomeUtente;
+document.getElementById("eta").innerHTML = `La tua password è ${password}`;
